4df2db91510f34da1445b4b4b0a5656e62e0de37
Isaac Farciert <rfisaac2296@yahoo.com>
2021-07-16 16:29:21 -0400

    add keys to support route transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <AnimatePresence exitBeforeEnter>
-      <Component {...pageProps} />
+      <Component {...pageProps} key={ router.pathname } />
     </AnimatePresence>
   );
 }
--- pages/menu.js
+++ pages/menu.js
@@ -3,6 +3,8 @@ import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
+import { motion } from 'framer-motion';
+
 import GetMenuStrings from './../shared/graphql-cms/GetMenuStrings';
 
 export default function Home({ strings, locale, locales}) {
@@ -10,7 +12,12 @@ export default function Home({ strings, locale, locales}) {
 
 
   return (
-    <div className={styles.container}>
+    <motion.div 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className={styles.container}
+    >
       
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -23,7 +30,7 @@ export default function Home({ strings, locale, locales}) {
 
       <footer className={styles.footer}>
       </footer>
-    </div>
+    </motion.div>
   )
 }
 
