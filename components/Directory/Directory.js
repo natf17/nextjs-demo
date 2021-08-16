bcc5db2ec3b7249da320ed53f4750e717727575d
Isaac Farciert <rfisaac2296@yahoo.com>
2021-08-16 17:36:04 -0400

    design basic map layout

--- components/Directory/Directory.js
+++ components/Directory/Directory.js
@@ -2,24 +2,42 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
+import AmenityBtn from './components/AmenityBtn';
+
 
 
 
 export default function Map({ strings, locale, locales}) {
   let router = useRouter();
-
+  console.log(strings);
 
   return (
     <motion.div 
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
+      className='self-stretch w-full'
     >
       
-      <main>
-        <h1>
-        { strings.pageTitle }
+      <main className='bg-green-100 h-full'>
+        <h1 className='text-4xl text-center p-2'>
+          { strings.pageTitle }
         </h1>
+
+        {/* Search widgets */}
+        <div className='bg-yellow-200'>
+          <h4>{ strings.tapWidget.instructions }</h4>
+          <div className='bg-yellow-300
+            flex p-2
+          '>          
+            <AmenityBtn label={ strings.tapWidget.br_label } />
+            <AmenityBtn label={ strings.tapWidget.water_label } />
+            <AmenityBtn label={ strings.tapWidget.firstaid_label } />
+            <AmenityBtn label={ strings.tapWidget.donations_label } />
+          </div>
+        </div>
+
+
         <Link href="/">
           <a>Home</a>
         </Link>
---
+++ components/Directory/components/AmenityBtn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function AmenityBtn({label}) {
+  return (
+    <div>
+      <div className='rounded-full bg-green-50 h-16 w-16
+        flex items-center justify-center'>      
+      </div>
+
+      <div className='text-center'>{label}</div>
+    </div>
+    
+  )
+}
