b4202a384a16a17ea3d1021e5d4b425446310ecd
Isaac Farciert <rfisaac2296@yahoo.com>
2021-08-27 11:47:51 -0400

    convert to ts

---
+++ pages/menu.tsx
@@ -0,0 +1,67 @@
+import makeGraphQLRequest from '../utils/makeGraphQLRequest';
+import GetMenuStrings from '../shared/queries/GetMenuStrings';
+import Menu from '../components/Menu';
+
+// Type data
+import { GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+
+// API Schema
+export type MenuStringData = {
+  pageTitle: string,
+  directory: {
+    title: string,
+    menuItems?: MenuItems[]
+  },
+  events: {
+    title: string,
+    menuItems?: MenuItems[]
+  }
+}
+// should this be moved to request models? / graphql schemas?
+export type MenuItems = {
+  id: number | string,
+  isVisible: boolean,
+  label: string,
+  url: string,
+  image?: {
+    url: string,
+    width: number,
+    height: number
+  }
+}
+
+// Page component props
+export type Props = {
+  strings: MenuStringData,
+  locale: string
+}
+
+// Next 'context' params
+export interface Params extends ParsedUrlQuery {
+  locale: string
+}
+
+
+export const getStaticProps: GetStaticProps<Props, Params> = async (context) => {
+  const locale = context.locale!;
+  
+  try {
+    const { menuPage } = await makeGraphQLRequest(locale, GetMenuStrings);
+    return {
+      props: {
+        strings: menuPage,
+        locale
+      }
+    }
+  } catch (error) {
+    // if any errors, return 404
+    return {
+      notFound: true
+    }
+  }
+}
+
+
+/* EXPORT COMPONENT */
+export default Menu;
