13fb47629bd69a51248c0dc0e91f8ef60502289e
Isaac Farciert <rfisaac2296@yahoo.com>
2021-08-30 16:55:38 -0400

    cleanup

--- pages/directory.tsx
+++ pages/directory.tsx
@@ -50,24 +50,6 @@ export interface Params extends ParsedUrlQuery {
   locale: string
 }
 
-// export const getStaticProps: GetStaticProps<Props, Params> = async (context) => {
-//   const locale = context.locale!;
-  
-//   try {
-//     const { menuPage } = await makeGraphQLRequest(locale, GetMapStrings);
-//     return {
-//       props: {
-//         strings: menuPage,
-//         locale
-//       }
-//     }
-//   } catch (error) {
-//     // if any errors, return 404
-//     return {
-//       notFound: true
-//     }
-//   }
-// }
 
 export const getStaticProps: GetStaticProps<Props, Params> = async (context) => {
   const locale = context.locale!;
@@ -117,65 +99,6 @@ export const getStaticProps: GetStaticProps<Props, Params> = async (context) => 
   }
 }
 
-
-/* SERVER SIDE CONFIG */
-// export async function getStaticProps({ locale, locales }) {
-//   // make request for strings on Strapi
-//   const data = await makeGraphQLRequest(locale, GetMapStrings);
-
-//   // make requests for amenities locations
-//   const bathroomLocations = await makeGraphQLRequest(locale, GetBathroomLocations);
-//   const waterFtnLocations = await makeGraphQLRequest(locale, GetWaterFountainLocations);
-//   const firstAidLocations = await makeGraphQLRequest(locale, GetFirstAidLocations);
-//   const donationLocations = await makeGraphQLRequest(locale, GetDonationLocations);
-
-
-//   // handle request errors with 404
-//   if (!data || !data.mapPage) {
-//     return { notFound: true }
-//   }
-
-//   // TODO: require ALL requests to resolve, else return error page
-
-//   // TEST: build composite amenity object with merged data
-//   const testCompositeAmenities = {
-//     bathrooms: {
-//       id: 'bathrooms',
-//       ...data.mapPage.bathroomAmenity,
-//       locations: [...bathroomLocations.bathrooms],
-//     },
-//     waterFountains: {
-//       id: 'waterFountains',
-//       ...data.mapPage.waterFountainAmenity,
-//       locations: [...waterFtnLocations.waterFountains],
-//     },
-//     firstAid: {
-//       id: 'firstAid',
-//       ...data.mapPage.firstAidAmenity,
-//       locations: [...firstAidLocations.firstAids],
-//     },
-//     donations: {
-//       id: 'donations',
-//       ...data.mapPage.donationAmenity,
-//       locations: [...donationLocations.donations],
-//     }
-//   }
-
-
-//   // pass down data into component props
-//   return {
-//     props: {
-//       strings: data.mapPage,
-//       amenityData: testCompositeAmenities,
-//       bathroomLocations: bathroomLocations.bathrooms,
-//       waterFtnLocations: waterFtnLocations.waterFountains,
-//       firstAidLocations: firstAidLocations.firstAids,
-//       donationLocations: donationLocations.donations,
-//       locale,
-//       locales
-//     }
-//   }
-// }
 
 
 
