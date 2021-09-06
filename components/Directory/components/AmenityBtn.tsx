e453674edbd94a0d177b23312cdd41576225b40c
Isaac Farciert <rfisaac2296@yahoo.com>
2021-09-06 12:41:45 -0400

    test material icons for buttons

--- components/Directory/components/AmenityBtn.tsx
+++ components/Directory/components/AmenityBtn.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { AmenityId } from '../../../pages/directory';
 
+// import the icons
+import { WcSharp as BathroomIcon } from '@material-ui/icons';
+import { LocalDrinkSharp as WaterFtnIcon } from '@material-ui/icons';
+import { LocalHospitalSharp as FirstAidIcon } from '@material-ui/icons';
+import { AttachMoneySharp as DonationsIcon } from '@material-ui/icons';
+
+
 export type Props = {
   label: string,
   onClick: (amenityId: AmenityId) => void,
@@ -11,7 +18,22 @@ export default function AmenityBtn({label, onClick, amenityId}: Props) {
   return (
     <div onClick={onClick.bind(null, amenityId)}>
       <div className='rounded-full bg-green-50 h-16 w-16
-        flex items-center justify-center'>      
+        flex items-center justify-center'>
+          { amenityId === "bathrooms" &&
+            <BathroomIcon fontSize="inherit" />
+          }
+          
+          { amenityId === "waterFountains" &&
+            <WaterFtnIcon fontSize="inherit" />
+          }
+
+          { amenityId === "firstAid" && 
+            <FirstAidIcon fontSize="inherit" />
+          }
+
+          { amenityId === "donations" && 
+            <DonationsIcon fontSize="inherit" />
+          }
       </div>
 
       <div className='text-center text-sm'>{label}</div>
