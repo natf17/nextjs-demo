67d8f91a78442789034d438810f59f9df0b2046e
Isaac Farciert <rfisaac2296@yahoo.com>
2021-09-30 17:03:01 -0400

    provide basic styling and click functionality

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -16,12 +16,12 @@ export default function Home({ strings }:Props) {
     >
       <Head><title>{strings.pageTitle}</title></Head>
       
-      <Link href="/menu">
-        <a className='block h-full'>
-          <h1 className='text-center text-6xl'>{strings.welcomeText}</h1>
-          <h3 className='text-center text-3xl'>{strings.tapToContinuePrompt}</h3>                  
-        </a>
-      </Link>
+      <Link href="/menu"><a className='block h-full'>
+        <div className='h-full'>
+        <h1 className='text-center text-6xl'>{strings.welcomeText}</h1>
+        <h3 className='text-center text-3xl'>{strings.tapToContinuePrompt}</h3>                  
+        </div>
+      </a></Link>
     </motion.div>
   )
 }
