3e14316efa63b63d23794d8e0fb2d4afc5818056
Isaac Farciert <rfisaac2296@yahoo.com>
2021-09-30 18:09:43 -0400

    position and style the logo

--- components/shared/Layout/components/Header.tsx
+++ components/shared/Layout/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
   src: string,
@@ -7,19 +8,24 @@ type Props = {
   height: number
 }
 
-export default function Header({src, width, height}:Props) {
+export default function Header({src, width, height}:Props) {  
   return (
-    <div>
+    <div className='overflow-hidden h-16
+      flex justify-center align-middle
+    '>
       {/* To include text: we need some way to make this dynamic, i.e.
       to refresh this information when site language
       changes, etc. */}
-      
-      <Image 
-        src={src} 
-        alt='logo'
-        width={width}
-        height={height}
-      />
+
+      <Link href='/'><a className='flex w-full justify-center align-middle'>
+        <Image 
+          src={src} 
+          alt='logo'
+          width={width}
+          height={height}          
+          layout={'intrinsic'}
+        />
+      </a></Link>
     </div>
   )
 }
