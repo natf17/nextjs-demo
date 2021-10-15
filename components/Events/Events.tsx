eeaedc79c8a24a43450a03b304958ad693ae16df
Isaac Farciert <rfisaac2296@yahoo.com>
2021-10-15 12:22:49 -0400

    create sorting and filtering fns

--- components/Events/Events.tsx
+++ components/Events/Events.tsx
@@ -1,42 +1,114 @@
 import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import EventGroup from './components/EventGroup';
 
 // types
 import { Props } from '../../pages/events';
 import { EventSeason, SeasonalEvent, SeasonalType } from '../../shared/models/GetEventData';
+// sort events into event types
+type EventsByType = {REG: SeasonalEvent[], CACO: SeasonalEvent[], CABR: SeasonalEvent[], other: SeasonalEvent[]};
+type FirstSeasonsByType = {REG?: EventSeason, CACO?: EventSeason, CABR?: EventSeason, other?: EventSeason};
 
 
+
+function getFirstSeasonOfType(
+  seasons: EventSeason[],
+  seasonType: SeasonalType
+): EventSeason | undefined {
+  console.log('RUN getFirstSeasonOfType()')
+  return seasons.find((s) => s.type === seasonType);
+}
 
 
 export default function Events({ strings, eventSeasons, seasonalEvents, locale}: Props) {
   // use locale lang as default eventLang
-  const [eventLang, setEventLang] = useState(locale);
-  const [filteredByLangEvents, setFilteredByLangEvents] = useState<SeasonalEvent[] | null>(null);
+  const [eventLangFilter, setEventLangFilter] = useState<string | null>(null);
+  const [visibleEvents, setVisibleEvents] = useState<SeasonalEvent[] | null>(null);
+  const [dummyCounter, setDummyCounter] = useState(0);
+  
 
-  // filter events to match selected lang
+  // calculate available languages from events
+  const availableLangs = useMemo(() => {
+    console.log('running fn 1')
+    let langs:string[] = [];
+    seasonalEvents.map((e) => {
+      if (!langs.includes(e.eventLanguage)) {
+        langs.push(e.eventLanguage);
+      }
+    });
+
+    return langs;
+  }, [seasonalEvents])
+
+
+  // sort events into event types
+  const eventsByType = useMemo(() => {
+    let sorted:EventsByType = {REG: [], CACO: [], CABR: [], other: []}
+    if (visibleEvents) {
+      visibleEvents.forEach((event) => {
+        switch (event.seasonalType) {
+          case 'REG':
+            sorted.REG.push(event);
+            break;
+
+          case 'CACO':
+            sorted.CACO.push(event);
+            break;
+
+          case 'CABR': 
+            sorted.CABR.push(event);
+            break;
+
+          case 'OTHER':
+            sorted.other.push(event);
+            break;
+        }
+      });      
+    }
+
+    return sorted;
+  }, [visibleEvents]);
+
+
+  // extract event seasons
+  const firstSeasonsByType = useMemo(()=> {
+    let seasonsByType: FirstSeasonsByType = {}
+
+    seasonsByType.REG = eventSeasons.find((s) => s.type === 'REG');
+    seasonsByType.CACO = eventSeasons.find((s) => s.type === 'CACO');
+    seasonsByType.CABR = eventSeasons.find((s) => s.type === 'CABR');
+    seasonsByType.other = eventSeasons.find((s) => s.type === 'OTHER');
+
+    return seasonsByType;
+
+  }, [eventSeasons])
+
+
+  // availableEventLangs: if available, filter events by locale language by default
+  useEffect(() => {
+    if (availableLangs.includes(locale)) {
+      setEventLangFilter(locale);
+    }    
+  }, [availableLangs, locale])
+
+
+  // eventLang: filter events to match selected lang
   useEffect(() => {    
-    const filtered = seasonalEvents.filter((event) => event.eventLanguage === eventLang);
-    setFilteredByLangEvents(filtered);    
-  }, [seasonalEvents, eventLang])
-
-
-  function getFirstSeasonOfType(
-    seasonType: SeasonalType
-  ): EventSeason | undefined {
-    return eventSeasons.find((s) => s.type === seasonType)
-  }
-
-  function filterEventsByType(
-    seasonType: SeasonalType
-  ): SeasonalEvent[] | [] {
-
-    if (!filteredByLangEvents) {
-      return []
+    if (!eventLangFilter) {
+      setVisibleEvents(seasonalEvents);
     }
     
-    return filteredByLangEvents.filter((e)=> e.seasonalType === seasonType);    
-  }
+    else {
+      const filtered = seasonalEvents.filter((event) => event.eventLanguage === eventLangFilter);
+      setVisibleEvents(filtered);
+    }
+    
+  }, [seasonalEvents, eventLangFilter])
+
+
+  
+
+  
 
   return (
     <motion.div 
@@ -49,30 +121,46 @@ export default function Events({ strings, eventSeasons, seasonalEvents, locale}:
       <main className='bg-green-100 h-full'>
         <h1 className='text-4xl text-center p-2'>{strings.pageTitle}</h1>
 
-        <div>{strings.eventLangPickerLabel} <span id="">{eventLang}</span></div>
+        <div>{strings.eventLangPickerLabel} <span id="">{eventLangFilter}</span></div>
+        {availableLangs.map((lang) => {
+          return (
+            <div key={lang}>{lang}</div>
+          )
+        })}
+
+        <br />
+
+        <div> 
+          <h1 className='text-xl'>Dummy counter</h1>
+          <div>
+            <span>{dummyCounter}</span><br />
+            <button onClick={()=> {setDummyCounter(dummyCounter - 1)}}>-1</button>
+            <button onClick={()=> {setDummyCounter(dummyCounter + 1)}}>+1</button>
+          </div>
+        </div>
         
         <EventGroup 
           title={strings.sectionRegCo.title}
-          eventSeason={getFirstSeasonOfType('REG')}
-          events={filterEventsByType('REG')}
+          eventSeason={firstSeasonsByType.REG}
+          events={eventsByType.REG}
         />
 
         <EventGroup 
           title={strings.sectionCACO.title}
-          eventSeason={getFirstSeasonOfType('CACO')}
-          events={filterEventsByType('CACO')}
+          eventSeason={undefined}
+          events={[]}
         />
 
         <EventGroup 
           title={strings.sectionCABR.title}
-          eventSeason={getFirstSeasonOfType('CABR')}
-          events={filterEventsByType('CABR')}
+          eventSeason={undefined}
+          events={[]}
         />
 
         <EventGroup 
           title={strings.sectionOtherEvents.title}
-          eventSeason={getFirstSeasonOfType('OTHER')}
-          events={filterEventsByType('OTHER')}
+          eventSeason={undefined}
+          events={[]}
         />
       </main>
     </motion.div>
