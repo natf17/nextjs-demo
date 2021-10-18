73f3742997ceadb7c335eaaae2047044661f13b3
Isaac Farciert <rfisaac2296@yahoo.com>
2021-10-18 15:55:25 -0400

    fix bg image positioning

--- pages/_app.js
+++ pages/_app.js
@@ -18,8 +18,8 @@ const globalValues = {
 
 const imageWrapperStyle = {
   position: 'fixed',
-  height: '100vh',
-  width: '100vw',
+  height: '100vh', width: '100vw',
+  top: 0, left: 0, right: 0, bottom: 0,
   overflow: 'hidden',
   zIndex: -1
 }
