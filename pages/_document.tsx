af471da3037eca96fb90a850476ee3d916b499dd
Isaac Farciert <rfisaac2296@yahoo.com>
2021-10-18 14:57:46 -0400

    import Roboto and modify Poppins weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link 
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" 
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;800&family=Roboto&display=swap" 
             rel="stylesheet" 
           />
         </Head>
