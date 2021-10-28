444b889a58e4cd5384d2e3fe4895b014d56020dc
Isaac Farciert <rfisaac2296@yahoo.com>
2021-10-28 14:53:12 -0400

    add basic hide/show functionality on events view

--- components/Events/components/EventGroup.tsx
+++ components/Events/components/EventGroup.tsx
@@ -1,8 +1,10 @@
 import { EventSeason, SeasonalEvent } from "../../../shared/models/GetEventData";
 import { GeneralStrings } from "../../../shared/models/GetEventsPageStrings";
 import { ControlPoint } from "@material-ui/icons";
+import { motion } from 'framer-motion';
 import SeasonInfo from "./SeasonInfo";
 import EventMonthsLayout from "./EventMonthsLayout";
+import { useState } from "react";
 
 export type Props = {
   title: string,
@@ -12,15 +14,32 @@ export type Props = {
 }
 
 export default function EventGroup({title, eventSeason, events, stringsGen}: Props) {
+  const [isGroupExpanded, setIsGroupExpanded] = useState(false);
+
+  const variants = {
+    expanded: { opacity: 1, height: "100%", display: "block" },
+    collapsed: { opacity: 0, height: "0", transitionEnd: { display: "none"}}
+  };
+  
+
+  const toggleExpanded: () => void = () => {
+    console.log('toggleExpanded() called')
+    setIsGroupExpanded(!isGroupExpanded);
+  }
+
   return (
     <article>
       {/* Event season name */}
-      <h2 className='text-3xl uppercase'>
+      <h2 className='text-3xl uppercase' onClick={ () => toggleExpanded() }>
         <ControlPoint fontSize="inherit" color="inherit" /> {title}
       </h2>
 
       {/* Event data */}
-      <div className="border-l p-2 pl-4">
+      <motion.div className="border-l pl-4"
+        animate={ isGroupExpanded ? "expanded" : "collapsed" }
+        variants = { variants }
+        initial = { false }
+      >
         {/* Event season information */}
         {eventSeason && 
           <SeasonInfo 
@@ -38,7 +57,7 @@ export default function EventGroup({title, eventSeason, events, stringsGen}: Pro
           ? <EventMonthsLayout events={events} /> 
           : <>{stringsGen.noEventsFound}</>
         }
-      </div>
+      </motion.div>
     </article>
   )
 }
