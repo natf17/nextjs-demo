a776498679a9791072c362a1cae76bd139a414ce
Isaac Farciert <rfisaac2296@yahoo.com>
2021-10-30 18:37:05 -0400

    new files: sticky footer

---
+++ components/shared/Layout/components/EndSession.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function EndSession() {
+  return (
+    <button className='rounded-full py-3 px-6 border border-red-600 bg-black bg-opacity-30 text-yellow-50'>
+      End Session
+    </button>
+  )
+}
