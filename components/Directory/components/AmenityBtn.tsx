55b814f72c5f8140397ef966afb4c1a63264cfbb
Isaac Farciert <rfisaac2296@yahoo.com>
2021-11-01 14:51:54 -0400

    redesign button layout

--- components/Directory/components/AmenityBtn.tsx
+++ components/Directory/components/AmenityBtn.tsx
@@ -17,28 +17,36 @@ export type Props = {
 
 export default function AmenityBtn({label, onClick, amenityId, selected}: Props) {
   return (
-    <div onClick={onClick.bind(null, amenityId)} className='cursor-pointer'>
-      <div className={`rounded-full bg-green-50 h-16 w-16
-        ${selected && 'border-4 border-blue-400'}
-        flex items-center justify-center`}>
-          { amenityId === "bathrooms" &&
-            <BathroomIcon fontSize="inherit" />
-          }
-          
-          { amenityId === "waterFountains" &&
-            <WaterFtnIcon fontSize="inherit" />
-          }
+    <div 
+      onClick={onClick.bind(null, amenityId)} 
+      className='cursor-pointer flex flex-col'
+    >
+      <div className='flex justify-center'>
+        <div 
+          className={`
+            rounded-full bg-gray-900 h-16 w-16 border-4 border-gray-500
+            ${selected && 'border-green-600 text-green-400'}
+            flex items-center justify-center text-3xl
+          `}
+        >        
+            { amenityId === "bathrooms" &&
+              <BathroomIcon fontSize="inherit" />
+            }
+            
+            { amenityId === "waterFountains" &&
+              <WaterFtnIcon fontSize="inherit" />
+            }
 
-          { amenityId === "firstAid" && 
-            <FirstAidIcon fontSize="inherit" />
-          }
+            { amenityId === "firstAid" && 
+              <FirstAidIcon fontSize="inherit" />
+            }
 
-          { amenityId === "donations" && 
-            <DonationsIcon fontSize="inherit" />
-          }
+            { amenityId === "donations" && 
+              <DonationsIcon fontSize="inherit" />
+            }        
+        </div>
       </div>
-
-      <div className='text-center text-sm'>{label}</div>
+      <div className='text-center text-base'>{label}</div>
     </div>
     
   )
