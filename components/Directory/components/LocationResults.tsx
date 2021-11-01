c58f7c8ef90e423fdfae6319aa90a4379f7d6125
Isaac Farciert <rfisaac2296@yahoo.com>
2021-11-01 17:20:06 -0400

    readability: improve bg contrast

--- components/Directory/components/LocationResults.tsx
+++ components/Directory/components/LocationResults.tsx
@@ -12,7 +12,7 @@ export type Props = {
 
 export default function LocationResults({ amenityTitle, locations }: Props) {
   return (
-    <div className='bg-gray-800 bg-opacity-30 px-1 rounded-tl-lg'>
+    <div className='bg-gray-500 bg-opacity-30 px-1 rounded-tl-lg'>
       <header className='text-3xl p-4 text-green-400'> { amenityTitle } </header>
 
       <div className='divide-y divide-gray-500'>
