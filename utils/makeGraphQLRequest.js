d1805a4077040e2c1cbbcdddbea6b0cad049fdcb
Isaac Farciert <rfisaac2296@yahoo.com>
2021-11-08 20:52:59 -0500

    add support for graphql jwt token for accesing production cms

--- utils/makeGraphQLRequest.js
+++ utils/makeGraphQLRequest.js
@@ -1,12 +1,38 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { 
+  ApolloClient,
+  createHttpLink, 
+  InMemoryCache 
+} from '@apollo/client';
 
 export default async function makeGraphQLRequest(locale, query) {
+  let client;
 
     // connect to GraphQL server
-    const client = new ApolloClient({
-      uri: process.env.CMS_GRAPHQL_ENDPOINT,
-      cache: new InMemoryCache()
-    });
+    // Dev environment - no token necessary
+    if (process.env.NODE_ENV === 'development') {
+      client = new ApolloClient({
+        ssrMode: true,
+        link: createHttpLink({
+          uri: process.env.CMS_GRAPHQL_ENDPOINT,          
+        }),      
+        cache: new InMemoryCache()
+      });  
+    }
+
+    // Production - add JWT access token
+    else {
+      client = new ApolloClient({
+        ssrMode: true,
+        link: createHttpLink({
+          uri: process.env.CMS_GRAPHQL_ENDPOINT,
+          headers: {
+            authorization: `Bearer ${process.env.CMS_ACCESS_TOKEN}`
+          }
+        }),      
+        cache: new InMemoryCache()
+      });
+    }
+    
 
     // initiate request
     // NOTE: All configured UTS locales inside of Next.js match exactly with Strapi's
