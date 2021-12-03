120a8ead845a60e807c26ba5251e9348d3b72a81
Isaac Farciert <rfisaac2296@yahoo.com>
2021-12-03 15:01:31 -0500

    setup first test

---
+++ components/Home/Home.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Home, {Props} from '../../pages/index';
+
+describe('Home Screen', () => {
+  const MOCK_PROPS: Props = {
+    locale: 'en',
+    strings: {
+      id: '1',
+      pageTitle: 'Test page title',
+      welcomeText: 'Welcome',
+      tapToContinuePrompt: 'Tap to continue'      
+    }    
+  }
+
+
+  it('renders input data without crashing', ()=> {
+    render(<Home {...MOCK_PROPS} />);
+    expect(screen.getByRole('heading', {name: MOCK_PROPS.strings.welcomeText})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: MOCK_PROPS.strings.tapToContinuePrompt})).toBeInTheDocument();    
+  });
+});
