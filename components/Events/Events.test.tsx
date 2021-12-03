e1151794a2fe53bca706e72a511046ea56832666
Isaac Farciert <rfisaac2296@yahoo.com>
2021-12-03 16:38:04 -0500

    add more basic render tests

---
+++ components/Events/Events.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Events, {Props} from '../../pages/events';
+
+describe('Events Screen', ()=> {
+
+  it('renders input data', ()=> {
+    
+  });
+});
+
+describe('Events language picker', () => {  
+  it('shows all event languages available', ()=> {
+    
+  });
+
+
+  it('correctly filters displayed events based on language selection', ()=> {
+
+  });
+
+  
+  it('does not render if no event languages exist', () => {
+
+  });
+
+
+  it('displays available language if only one event language is available', () => {
+
+  });
+
+
+  it('disables language selection if only one event language is available', () => {
+
+  });
+});
---
+++ components/Menu/Menu.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Menu, {Props} from '../../pages/menu';
+
+describe('Home Screen', () => {
+  const MOCK_PROPS: Props = {
+    locale: 'en',
+    strings: {
+      pageTitle: 'Page title demo',
+      directory: {
+        title: 'Map section',
+        menuItems: [{
+            id: '1',
+            isVisible: true,
+            label: 'Menu item 1',
+            url: ''  
+        }]
+      },
+      events: {
+        title: 'Events section',
+        menuItems: [{
+          id: '1',
+          isVisible: true,
+          label: 'Menu item 1',
+          url: ''          
+        }]
+      }
+    }    
+  }
+
+
+  it('renders input data without crashing', ()=> {
+    render(<Menu {...MOCK_PROPS} />);
+    expect(screen.getByRole('heading', { name: MOCK_PROPS.strings.pageTitle })).toBeInTheDocument();
+  });
+});
