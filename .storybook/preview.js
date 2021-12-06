43e15c33cc63b979002318c66d0fba7611b64c19
Isaac Farciert <rfisaac2296@yahoo.com>
2021-12-06 16:03:37 -0500

    add Storybook compatible NextJS router mock

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import '../styles/tailwind.css';
+import { RouterContext } from "next/dist/next-server/lib/router-context"; // next 11, 12 https://storybook.js.org/docs/react/writing-stories/parameters
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,5 +8,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
   },
 }
--- components/Events/Events.stories.tsx
+++ components/Events/Events.stories.tsx
@@ -3,19 +3,48 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Events from './Events';
 
 export default {
-  title: 'Events Screen',
+  title: 'Events/Events Screen',
   component: Events
 } as ComponentMeta<typeof Events>;
 
 
 const Template: ComponentStory<typeof Events> = (args) => <Events {...args} />;
 
-export const FirstStory = Template.bind({});
+export const ScreenWithEvents = Template.bind({});
 
-FirstStory.args = {
-  eventSeasons:[],
+ScreenWithEvents.args = {
   locale: 'en',
-  seasonalEvents: [],
+  eventSeasons:[{
+    "seasonYears": "2021",
+    "serviceYear": 2020,
+    "type": "REG",
+    "theme": "Powerful by Faith!",
+    "id": "1",
+    "durationDays": 3
+  },
+  {
+    "seasonYears": "2021-2022",
+    "serviceYear": 2021,
+    "type": "CACO",
+    "theme": "\"Excercise Faith!\"",
+    "id": "3",
+    "durationDays": 1
+  },
+  {
+    "seasonYears": "2021-2022",
+    "serviceYear": 2021,
+    "type": "CABR",
+    "theme": "Strengthen Your Faith!",
+    "id": "5",
+    "durationDays": 1
+  }],  
+  seasonalEvents: [{
+   eventLanguage: 'en',
+   event_season: {seasonYears: '2020-2021', serviceYear: 2021, type: 'REG', theme: 'Convention theme!'},
+   id: '12349876',
+   seasonalType: 'REG',
+   startDate: '2021-06-02'
+  }],
   strings: {
     pageTitle: 'Events',
     pageDescription: 'Page description',
