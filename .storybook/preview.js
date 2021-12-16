fc4ebb8737e166edcba7d6383b8e80a7261b07b2
Isaac Farciert <rfisaac2296@yahoo.com>
2021-12-16 14:27:21 -0500

    upgrade NextJS version, path location

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import '../styles/tailwind.css';
-import { RouterContext } from "next/dist/next-server/lib/router-context"; // next 11, 12 https://storybook.js.org/docs/react/writing-stories/parameters
+import { RouterContext } from "next/dist/shared/lib/router-context"; // next 11, 12 https://storybook.js.org/docs/react/writing-stories/parameters
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
