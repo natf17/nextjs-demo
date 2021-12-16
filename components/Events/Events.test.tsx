1ba6be282d9b6ca710fbcbbba13da84a44d1cca4
Isaac Farciert <rfisaac2296@yahoo.com>
2021-12-16 14:28:54 -0500

    mock useRouter with Jest

--- components/Events/Events.test.tsx
+++ components/Events/Events.test.tsx
@@ -2,20 +2,44 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/testing-react';
 import * as stories from './Events.stories'
-import * as globalConfig from './../../.storybook/preview';
 
+import { useRouter } from 'next/router';
 
-// Failing for some reason, the composeStories() is not correctly setting
-// useRouter() context. useRouter() returns null, whereas inside Storybook, 
+// Storybook composeStories() is not correctly setting the useRouter context
+// useRouter() returns null, whereas inside Storybook, 
 // it correctly returns the mock Router context that storybook-addon-next-router enables
 // This causes an error when running this test, as the render() function cannot render the
 // Event.tsx component, which attempts to destructure the useRouter() return object {locale}
-const { WithSeasonsAndEvents } = composeStories(stories, globalConfig);
+// Mocking in Jest...
+/*
+--- Can also be mocked and spied like this:
+    jest.spyOn(require('next/router'), 'useRouter')
+        .mockImplementation(() => ({
+          locale: 'en'
+        })
+    );
+*/
 
+jest.mock('next/router', () => ({
+    __esModule: true,
+    ...jest.requireActual('next/router'),
+    useRouter: jest.fn(() => ({locale: 'en'}))
+  })
+);
+
+const { WithSeasonsAndEvents } = composeStories(stories);
 
 
 describe('Events Screen', ()=> {
   render(<WithSeasonsAndEvents />);
+
+  it('mocks useRouter and tracks calls to it', () => {
+
+    expect(useRouter).toHaveBeenCalled();
+
+  });
+
+
   it('renders input data', ()=> {
     
   });
