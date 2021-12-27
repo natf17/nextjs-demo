c3275c9ee1c0a7881690d83495cbe208b5f024bc
Isaac Farciert <rfisaac2296@yahoo.com>
2021-12-27 13:59:00 -0500

    add support for disabled state, and basic mouse/touch functionality

--- components/shared/ui/forms/OptionPicker.tsx
+++ components/shared/ui/forms/OptionPicker.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { KeyboardArrowDown as DownArrowIcon } from '@material-ui/icons';
 
 type OptionPickerProps = {
@@ -11,11 +11,23 @@ type OptionPickerProps = {
 export default function OptionPicker({initialValue, options, onSelect}: OptionPickerProps) {
   const [open, setOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState(initialValue);
-  
-  if (!options)           return null;
-  if (options.length < 1) return null;
+  let dropdownRef = useRef<HTMLUListElement>(null);
 
-  const moreThanOne = options.length > 1;
+  const captureClickOutside = (event:Event) => {
+    // check if click happened outside dropdown    
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      setOpen(false);
+    }
+  }
+
+  // runs on mount and unmount
+  useEffect(()=> {
+    document.addEventListener('click', captureClickOutside, false);
+
+    return () => {
+      document.removeEventListener('click', captureClickOutside, false);
+    }
+  })
 
   const handleSelect: (selectedValue: string) => void = (selectedValue) => {
     // update selection
@@ -25,30 +37,49 @@ export default function OptionPicker({initialValue, options, onSelect}: OptionPi
     // call callback
     onSelect(selectedValue);
   }
+
+  // don't render if no options are provided
+  if (!options)           return null;
+  if (options.length < 1) return null;
+  const moreThanOne = options.length > 1;
   
   return (
     <div>
       {/* Button */}
-      <button 
+      <button
+        onClick={ (e) => {
+          // prevent duplicate handlers (e.g. above document-level close handler)
+          // from interfering with open/close state
+          e.stopPropagation();
+          setOpen((prev) => !prev);
+        }}
         className={`
-          border border-green-600 rounded-md
-          px-2 py-2 text-green-300          
-        `}
-        onClick={() => moreThanOne && setOpen((prev) => !prev)}
+          border px-2 py-2 rounded-md         
+          ${moreThanOne ? `text-green-300`: `text-gray-400`}
+          ${moreThanOne ? `border-green-600`: `border-gray-400`}
+        `}        
       >
-        <div className={`inline-block max-w-sm overflow-hidden whitespace-nowrap overflow-ellipsis`}>
+        <div 
+          className={`
+            inline-block max-w-xs overflow-hidden 
+            whitespace-nowrap overflow-ellipsis align-top
+          `}
+        >
           { options.filter((option) => option.value === selectedValue)[0].label }
         </div>
-        { moreThanOne && <DownArrowIcon />}
+        
+        {/* Only show arrow if there are more options */}
+        { moreThanOne && <DownArrowIcon />}                        
       </button>
 
       {/* Dropdown */}
-      { open && 
+      { open && moreThanOne &&
         <ul className={`
             border border-green-800 text-green-400 
             rounded-md p-2 px-4 max-w-xs bg-black bg-opacity-50
             divide-y divide-green-800
           `}
+          ref={ dropdownRef }
         >
           { options.map((option) => (
               <li key={option.value}
