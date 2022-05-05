aea2886fb451644438cdb4b4cf0429da42111375
Isaac Farciert <rfisaac2296@yahoo.com>
2022-05-05 19:46:54 -0400

    run prettier

--- utils/makeGraphQLRequest.js
+++ utils/makeGraphQLRequest.js
@@ -1,48 +1,43 @@
-import { 
-  ApolloClient,
-  createHttpLink, 
-  InMemoryCache 
-} from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 
 export default async function makeGraphQLRequest(locale, query) {
   let client;
 
-    // connect to GraphQL server
-    // If token is necessary, include it
-    if (process.env.NODE_ENV === 'production' && process.env.CMS_ACCESS_TOKEN) {
-      client = new ApolloClient({
-        ssrMode: true,
-        link: createHttpLink({
-          uri: process.env.CMS_GRAPHQL_ENDPOINT,
-          headers: {
-            authorization: `Bearer ${process.env.CMS_ACCESS_TOKEN}`
-          }
-        }),      
-        cache: new InMemoryCache()
-      });        
-    }
-
-    // No token needed
-    else {
-      client = new ApolloClient({
-        ssrMode: true,
-        link: createHttpLink({
-          uri: process.env.CMS_GRAPHQL_ENDPOINT,          
-        }),      
-        cache: new InMemoryCache()
-      });
-    }
-    
-
-    // initiate request
-    // NOTE: All configured UTS locales inside of Next.js match exactly with Strapi's
-    const { data } = await client.query({
-      query: query(locale)
+  // connect to GraphQL server
+  // If token is necessary, include it
+  if (process.env.NODE_ENV === "production" && process.env.CMS_ACCESS_TOKEN) {
+    client = new ApolloClient({
+      ssrMode: true,
+      link: createHttpLink({
+        uri: process.env.CMS_GRAPHQL_ENDPOINT,
+        headers: {
+          authorization: `Bearer ${process.env.CMS_ACCESS_TOKEN}`,
+        },
+      }),
+      cache: new InMemoryCache(),
     });
+  }
 
-    return data;
+  // No token needed
+  else {
+    client = new ApolloClient({
+      ssrMode: true,
+      link: createHttpLink({
+        uri: process.env.CMS_GRAPHQL_ENDPOINT,
+      }),
+      cache: new InMemoryCache(),
+    });
+  }
+
+  // initiate request
+  // NOTE: All configured UTS locales inside of Next.js match exactly with Strapi's
+  const { data } = await client.query({
+    query: query(locale),
+    // variables: {locale}
+  });
+
+  return data;
 }
-
 
 /* ***
 APOLLO CONFIGURATION
