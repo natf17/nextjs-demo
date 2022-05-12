f94d11ae85511550fc8e988039dc7065712060e3
Isaac Farciert <rfisaac2296@yahoo.com>
2022-05-12 17:02:51 -0400

    feat: integrate new btn_text property for short event type labels

--- shared/models/GetEventsPageStrings.ts
+++ shared/models/GetEventsPageStrings.ts
@@ -1,37 +1,40 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 // separated types for easy type separation and import from modules
 export type EventsPageSchema = {
-  pageTitle: string,
-  pageDescription: string,
-  eventLangPickerLabel: string,
-  general: GeneralStrings,
+  pageTitle: string;
+  pageDescription: string;
+  eventLangPickerLabel: string;
+  general: GeneralStrings;
   sectionRegCo: {
-    title: string
-  },
+    title: string;
+    btn_text: string;
+  };
   sectionCACO: {
-    title: string
-  },
+    title: string;
+    btn_text: string;
+  };
   sectionCABR: {
-    title: string
-  },
+    title: string;
+    btn_text: string;
+  };
   sectionOtherEvents: {
-    title: string
-  }
-}
+    title: string;
+    btn_text: string;
+  };
+};
 
 export type GeneralStrings = {
-  eventThemeLabel: string,
-  durationLabel: string,
-  yearsShowingLabel: string,
-  dateLabel: string,
-  eventLangLabel: string,
-  noEventsFound: string
-}
-
+  eventThemeLabel: string;
+  durationLabel: string;
+  yearsShowingLabel: string;
+  dateLabel: string;
+  eventLangLabel: string;
+  noEventsFound: string;
+};
 
 const query = (locale = "en") => {
-  return gql`
+  return gql/* GraphQL */ `
     query {
       eventsPage(locale: "${locale}") {
         pageTitle
@@ -47,19 +50,23 @@ const query = (locale = "en") => {
         }
         sectionRegCo {
           title
+          btn_text
         }
         sectionCACO {
           title
+          btn_text
         }
         sectionCABR {
           title
+          btn_text
         }
         sectionOtherEvents {
           title
+          btn_text
         }
       }
     }  
   `;
-} 
+};
 
 export default query;
