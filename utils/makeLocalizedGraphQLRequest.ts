275b3d775c67db549e9d01e80f6bf43813ae01c4
Isaac Farciert <rfisaac2296@yahoo.com>
2022-05-19 18:07:14 -0400

    refactor: create new locale-friendly, GQL compliant requesting function and extract Apollo config

---
+++ utils/makeLocalizedGraphQLRequest.ts
@@ -0,0 +1,36 @@
+import { DocumentNode } from "@apollo/client";
+import getNewApolloClient from "./getNewApolloClient";
+
+export type LocalizedQuery = {
+  locale: string;
+  query: DocumentNode;
+  variables?: {
+    [index: string]: string;
+  };
+};
+
+export default async function makeLocalizedGraphQLRequest({
+  locale,
+  query,
+  variables,
+}: LocalizedQuery) {
+  // create a new Apollo client for each request to avoid stale cached data from previous requests
+  let client = getNewApolloClient();
+
+  // NOTE: All configured UTS locales inside of Next.js match exactly with Strapi's
+  const { data } = await client.query({
+    query: query,
+    variables: {
+      locale: locale,
+      ...variables,
+    },
+  });
+
+  return data;
+}
+
+/* ***
+APOLLO CONFIGURATION
+https://www.apollographql.com/docs/react/performance/server-side-rendering/#initializing-apollo-client
+https://www.apollographql.com/docs/react/networking/authentication/#header
+* ***/
