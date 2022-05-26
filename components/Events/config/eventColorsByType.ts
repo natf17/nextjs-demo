73680c06d1a0a7e19b0a5659dc4010b6b24bdad5
Isaac Farciert <rfisaac2296@yahoo.com>
2022-05-26 19:00:56 -0400

    feat: add event-type themed styles for text only

--- components/Events/config/eventColorsByType.ts
+++ components/Events/config/eventColorsByType.ts
@@ -3,27 +3,32 @@ import { EventGroupTypes } from "../Events";
 const eventColorsByType: {
   [key in EventGroupTypes]: {
     buttons: string;
+    text: string;
     month: string;
     event_tile: string;
   };
 } = {
   REG: {
     buttons: "shadow-cyan-500/50 text-blue-400",
+    text: "text-blue-400",
     month: "border-blue-400",
     event_tile: "bg-blue-900",
   },
   CACO: {
     buttons: "shadow-amber-500/50 text-yellow-400",
+    text: "text-yellow-400",
     month: "border-yellow-400",
     event_tile: "bg-yellow-900",
   },
   CABR: {
     buttons: "shadow-orange-500/50 text-orange-300",
+    text: "text-orange-300",
     month: "border-orange-300",
     event_tile: "bg-orange-900",
   },
   OTHER: {
     buttons: "shadow-cyan-500/50 text-blue-400",
+    text: "text-blue-400",
     month: "border-blue-400",
     event_tile: "bg-blue-900",
   },
