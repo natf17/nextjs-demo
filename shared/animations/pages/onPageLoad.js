cb1c27d909cd6815d40d4cb2a563625179cd57bc
Isaac Farciert <rfisaac2296@yahoo.com>
2022-06-04 09:18:01 -0400

    refactor: extract page load animations into separate file

--- shared/animations/pages/onPageLoad.js
+++ shared/animations/pages/onPageLoad.js
@@ -1,1 +1,9 @@
 // define page animations for when a page loads
+
+const MotionFadeEnter = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+};
+
+export { MotionFadeEnter };
