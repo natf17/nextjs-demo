b67ad9197dc4f5795db4667f53872d1bff9eded4
Isaac Farciert <rfisaac2296@yahoo.com>
2022-06-14 15:19:21 -0400

    refactor: to read from shared state store when selecting an map to show

--- components/Directory/components/DirectoryMap.tsx
+++ components/Directory/components/DirectoryMap.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import { AmenityId } from "../../../pages/directory";
 import {
   LocationSchema,
   MapImages,
@@ -8,22 +7,21 @@ import {
 import useMapUIStore from "../useMapUIStore";
 
 export type Props = {
-  selectedAmenity: AmenityId | undefined;
   maps: MapImages;
   locationData: LocationSchema[];
 };
 
-export default function DirectoryMap({ maps, locationData }: Props) {
+export default function DirectoryMap({ locationData }: Props) {
   const availableLevelsZZ = useMapUIStore((state) => state.availableLevels);
   const selectedLevelZZ = useMapUIStore((state) => state.selectedLevelName);
   const selectLevelZZ = useMapUIStore((state) => state.selectLevel);
 
-  const mapLevelMatch = locationData.filter(
+  const matchedLevelMap = locationData.find(
     (level) => level.level_name === selectedLevelZZ
-  )[0];
+  )?.map;
 
   return (
-    <div className="w-full max-w-6xl mx-auto drop-shadow-md py-6">
+    <div className="w-full max-w-6xl mx-auto drop-shadow-md">
       {/* Select level */}
       <div className="mt-10 px-2 text-center">
         {availableLevelsZZ?.map((level) => (
@@ -43,26 +41,22 @@ export default function DirectoryMap({ maps, locationData }: Props) {
         ))}
       </div>
 
-      {mapLevelMatch ? (
+      {matchedLevelMap ? (
         <Image
           src={`${
-            process.env.NEXT_PUBLIC_VERCEL_IMG_API + mapLevelMatch.map.url
+            process.env.NEXT_PUBLIC_VERCEL_IMG_API + matchedLevelMap.url
           }`}
           alt={""}
-          width={mapLevelMatch.map.width}
-          height={mapLevelMatch.map.height}
+          width={matchedLevelMap.width}
+          height={matchedLevelMap.height}
           layout="responsive"
           priority
         />
       ) : (
-        <Image
-          src={`${process.env.NEXT_PUBLIC_VERCEL_IMG_API + maps.default.url}`}
-          alt={""}
-          width={maps.default.width}
-          height={maps.default.height}
-          layout="responsive"
-          priority
-        />
+        <div className="px-4 py-8 min-h-[20em] text-gray-200 flex justify-center items-center">
+          {/* TODO: Add field in API, then fix hardcoded value here */}
+          <h3>ADD CMS: Map not available for selected level</h3>
+        </div>
       )}
     </div>
   );
