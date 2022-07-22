ff8a6186a9c91c7eac7ffc6c7338af0bd425738d
Isaac Farciert <rfisaac2296@yahoo.com>
2022-07-22 14:36:30 -0400

    feat: add new multi-locale home page string request fn

--- shared/models/GetHomePageStrings.ts
+++ shared/models/GetHomePageStrings.ts
@@ -1,12 +1,15 @@
-import { gql } from '@apollo/client';
-
+import { DocumentNode, gql } from "@apollo/client";
 
 export type HomePageSchema = {
-  id: string,
-  pageTitle: string,
-  tapToContinuePrompt: string,
-  welcomeText: string
-}
+  id: string;
+  pageTitle: string;
+  tapToContinuePrompt: string;
+  welcomeText: string;
+};
+
+export type MultiLangHomePage = {
+  [key: string]: HomePageSchema;
+};
 
 const query = (locale = "en") => {
   return gql`
@@ -21,4 +24,30 @@ const query = (locale = "en") => {
   `;
 };
 
+/* Query builder for requesting each configured locale in single query */
+const getMultiLangQuery = (locales: string[]): DocumentNode => {
+  let multiLangQuery = "";
+  locales.forEach((locale) => {
+    multiLangQuery += `
+      ${locale}: homePage(locale: "${locale}") {
+        ...homePageFields
+      }
+    `;
+  });
+
+  return gql`
+    fragment homePageFields on HomePage {
+      id
+      pageTitle
+      tapToContinuePrompt
+      welcomeText
+    }
+    
+    query MultiLangQuery {
+      ${multiLangQuery}
+    }
+  `;
+};
+
+export { getMultiLangQuery };
 export default query;
