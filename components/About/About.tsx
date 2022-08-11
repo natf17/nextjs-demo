c114a5a6004e545301474134cce5ac4e4c63de48
Isaac Farciert <rfisaac2296@yahoo.com>
2022-08-11 18:33:14 -0400

    feat: add support for markdown

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,5 +1,12 @@
-function About() {
-  return <>Hello</>;
+import { Props } from "./../../pages/about";
+import ReactMarkdown from "react-markdown";
+
+function About({ strings }: Props) {
+  return (
+    <main className="prose prose-invert">
+      <ReactMarkdown>{strings.richDescription}</ReactMarkdown>
+    </main>
+  );
 }
 
 export default About;
