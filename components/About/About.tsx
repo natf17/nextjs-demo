e441b233b40f787b80637b575ed55d0bb009f013
Isaac Farciert <rfisaac2296@yahoo.com>
2022-08-12 00:19:34 -0400

    change: apply standard heading styles

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -3,7 +3,8 @@ import ReactMarkdown from "react-markdown";
 
 function About({ strings }: Props) {
   return (
-    <main className="prose prose-invert">
+    <main className="prose prose-invert mx-auto">
+      <h1 className="text-center p-2 mb-4">{strings.pageTitle}</h1>
       <ReactMarkdown>{strings.richDescription}</ReactMarkdown>
     </main>
   );
