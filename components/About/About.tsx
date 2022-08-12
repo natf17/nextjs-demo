0d2c6f404d1e9b93a88a7904aa06132504c9ddd5
Isaac Farciert <rfisaac2296@yahoo.com>
2022-08-12 14:44:08 -0400

    fix: add image guard

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -6,17 +6,19 @@ function About({ strings }: Props) {
   return (
     <main className="prose prose-invert mx-auto">
       <h1 className="text-center p-2 mb-4">{strings.pageTitle}</h1>
-      <div className="max-w-xs mx-auto shadow-lg shadow-blue-500/40">
-        <Image
-          src={`${
-            process.env.NEXT_PUBLIC_VERCEL_IMG_API + strings.featImg.url
-          }`}
-          alt={strings.featImg.alternativeText}
-          width={strings.featImg.width}
-          height={strings.featImg.height}
-          layout="responsive"
-        />
-      </div>
+      {strings.featImg?.url && (
+        <div className="max-w-xs mx-auto shadow-lg shadow-blue-500/40">
+          <Image
+            src={`${
+              process.env.NEXT_PUBLIC_VERCEL_IMG_API + strings.featImg.url
+            }`}
+            alt={strings.featImg.alternativeText}
+            width={strings.featImg.width}
+            height={strings.featImg.height}
+            layout="responsive"
+          />
+        </div>
+      )}
       <ReactMarkdown>{strings.richDescription}</ReactMarkdown>
     </main>
   );
