a9aca78df6899a69dfbce5a82c25557181d48f27
Isaac Farciert <rfisaac2296@yahoo.com>
2022-08-15 15:03:38 -0400

    change: use new UI scroll component and add close button

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,27 +2,63 @@ import { Props } from "./../../pages/about";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { MotionFadeEnter } from "./../../shared/animations/pages/onPageLoad";
+import { MotionSlideUp } from "./../../shared/animations/pages/onPageLoad";
+import { CloseSharp } from "@mui/icons-material";
+import Link from "next/link";
+import OverflowScrollGradient from "../shared/Layout/components/OverflowScrollGradient";
 
 function About({ strings }: Props) {
   return (
-    <motion.main {...MotionFadeEnter} className="prose prose-invert mx-auto">
-      <h1 className="text-center p-2 mb-4">{strings.pageTitle}</h1>
-      {strings.featImg?.url && (
-        <div className="max-w-xs mx-auto shadow-lg shadow-blue-500/40">
-          <Image
-            src={`${
-              process.env.NEXT_PUBLIC_VERCEL_IMG_API + strings.featImg.url
-            }`}
-            alt={strings.featImg.alternativeText}
-            width={strings.featImg.width}
-            height={strings.featImg.height}
-            layout="responsive"
-          />
+    <motion.div
+      {...MotionSlideUp}
+      className="w-full self-stretch flex content-end"
+    >
+      <div
+        className="w-full max-w-3xl mx-auto self-end relative
+        overflow-hidden rounded-t-lg bg-slate-800/60
+      "
+      >
+        {/* Close button TODO: separate into a component */}
+        <div className={`absolute top-0 right-0 p-2 z-10`}>
+          <Link href="/menu">
+            <a>
+              <button className="border border-indigo-300 p-2 rounded-full hover:bg-slate-500">
+                <CloseSharp htmlColor="#f3f3f3" />
+              </button>
+            </a>
+          </Link>
         </div>
-      )}
-      <ReactMarkdown>{strings.richDescription}</ReactMarkdown>
-    </motion.main>
+
+        <OverflowScrollGradient
+          gradientHeight="3rem"
+          twTargetFadeColor="slate-800"
+        >
+          <div
+            className={`relative py-12 
+              max-h-[85vh] overflow-y-scroll`}
+          >
+            <main className="prose prose-invert mx-auto">
+              <h1 className="text-center mb-4">{strings.pageTitle}</h1>
+              {strings.featImg?.url && (
+                <div className="max-w-xs mx-auto shadow-lg shadow-blue-500/40">
+                  <Image
+                    src={`${
+                      process.env.NEXT_PUBLIC_VERCEL_IMG_API +
+                      strings.featImg.url
+                    }`}
+                    alt={strings.featImg.alternativeText}
+                    width={strings.featImg.width}
+                    height={strings.featImg.height}
+                    layout="responsive"
+                  />
+                </div>
+              )}
+              <ReactMarkdown>{strings.richDescription}</ReactMarkdown>
+            </main>
+          </div>
+        </OverflowScrollGradient>
+      </div>
+    </motion.div>
   );
 }
 
